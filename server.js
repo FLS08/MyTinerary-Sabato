99f3bf4fbdbfe4d94f835867ea51075871d8e4b7
Francisco-Sabato <franciscosabato08@gmail.com>
2022-04-01 01:34:40 -0300

    build

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 require('./config/database')
 const cors = require('cors')
 const express = require('express')
+const path = require('path')
+const passport = require('./config/passport')
 
 const Router = require('./routes/routes')
-const PORT = 4000
+
 
 const app = express()
 
@@ -12,7 +14,17 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(passport.initialize())
 app.use('/api', Router)
 
-app.listen(PORT,()=>console.log('Server ready on PORT ' + PORT))
+if (process.env.NODE_ENV === 'production') {
 
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res)=>{
+
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    })
+    
+}
+app.listen(process.env.PORT || 4000,process.env.HOST || '0.0.0.0' ,()=>console.log('Server ready on PORT ' + PORT))
